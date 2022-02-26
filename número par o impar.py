e56380744859f92cd82f95d76cd29416c0b85932
pyRuizSD <98649699+pyRamsd@users.noreply.github.com>
2022-02-26 01:03:02 -0500

    Add files via upload

---
+++ número par o impar.py
@@ -0,0 +1,11 @@
+print("""======================================================
+* programa que determina si un número es par o impar *
+======================================================
+- En caso de ingresar un número tipo float, saldrá un error en el programa -\n""")
+
+numero = int(input("Intruduzca un número entero: "))
+
+if numero % 2 == 0:
+    print("El número",numero, "es par")
+elif numero % 2 == 1:
+    print("El número", numero, "es impar")
