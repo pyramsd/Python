d41ec6fbf71552769e4bc47c25c04cca68cb1c25
pyRuizSD <98649699+pyRamsd@users.noreply.github.com>
2022-06-19 11:44:03 -0500

    Update cuadrado hueco de asteriscos.py

--- Algunos ejercicios de iteración y ciclo for/cuadrado hueco de asteriscos.py
+++ Algunos ejercicios de iteración y ciclo for/cuadrado hueco de asteriscos.py
@@ -1,6 +1,6 @@
 def cuadrado(n):
     for i in range(n):
         for j in range(n):
-            print("*" if(i==0 or i==n-1 or j==0 or j==n-1) else" ", end="")
+            print("*" if(i==0 or i==n-1 or j==0 or j==n-1) else" ", end=" ")
         print()
 cuadrado(10)
