0aa9ae96da6f1b08c4ca79e150f4dc1f6d1142df
pyRuizSD <98649699+pyRamsd@users.noreply.github.com>
2022-07-05 23:20:00 -0500

    Update area_de_un_circulo.py

--- area_de_un_circulo.py
+++ area_de_un_circulo.py
@@ -1,9 +1,9 @@
 import math
-print("Clcular área de un círculo")
+print("Calcular area de un circulo")
 radio = float(input("Intraduzca el valor del radio: "))
 area = math.pi * (radio ** 2)
 
 if radio < 1:
     print("El radio no puede ser 0 o menor que cero.")
 elif radio >= 1:
-    print(f"El área del círculo es {round(area, 2)}")
+    print(f"El area del circulo es {round(area, 2)}")
