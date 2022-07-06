6411b2c92ff895b9bede99f7391bba176e9123b5
pyRuizSD <98649699+pyRamsd@users.noreply.github.com>
2022-07-06 01:54:57 -0500

    Update and rename número par o impar.py to par_o_impar.py

--- par_o_impar.py
+++ par_o_impar.py
@@ -6,7 +6,7 @@ numero = int(input("Intruduzca un número entero: "))
 
 if numero % 2 == 0:
     print("El número",numero, "es par")
-elif numero % 2 == 1:
+else:
     print("El número", numero, "es impar")
 
 # - En caso de ingresar un número tipo float, saldrá un error en el programa -
