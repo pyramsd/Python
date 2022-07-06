c66326a0b597fa37176db3dd49ad28bbba4c5744
pyRuizSD <98649699+pyRamsd@users.noreply.github.com>
2022-07-06 00:18:56 -0500

    Update and rename caracteres totales de un texto.py to caracteres_totales_en_un_string.py

--- caracteres_totales_en_un_string.py
+++ caracteres_totales_en_un_string.py
@@ -1,6 +1,6 @@
 print("Cantidad de caracteres totles de un texto")
 
-text = input("Texto: ")
+text = input("Texto: ").lower()
 def num_caracteres(text):
     print(f"El texto '{text}' tiene {len(text)} carácteres")
 num_caracteres(text)
