6eea97d55ee6d7562b9ba75b42cdd50e5aabef35
Sergio Ruiz <98649699+pyramsd@users.noreply.github.com>
2023-02-27 18:50:15 -0500

    Update palabra_heterograma.py

--- palabra_heterograma.py
+++ palabra_heterograma.py
@@ -1,4 +1,4 @@
-# una palabra isograma una palabra que no se repitan letras
+# una palabra heterograma una palabra que no se repitan letras
 def is_isogram(string):
     string = string.lower().replace(" ","").replace("-", "")
     if len(string) == len(set(string)):
