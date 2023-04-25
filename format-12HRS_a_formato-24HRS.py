799062b73b44b24841dbcb7cf1ace6168cf38578
pyRamsd <minauzum56@gmail.com>
2023-04-25 18:22:50 -0500

    created file

--- format-12HRS_a_formato-24HRS.py
+++ format-12HRS_a_formato-24HRS.py
@@ -1,4 +1,16 @@
-def 12hrs_24hrs(hora):
+def convertir_horario(hora):
     
     _hora_ = hora.split(":")
-    
+    if hora[-2].lower() == "pm":
+        if _hora_[0] != "12":
+            _hora_[0] = str(int(_hora_[0]) + 12)
+
+    else:
+        if _hora_[0] == "12":
+            _hora_[0] = "00"
+
+    nueva_hora = ":".join(_hora_)
+
+    return nueva_hora
+
+print(convertir_horario("12:40PM"))
